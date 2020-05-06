32c41b7d20c226db2be2648dcc6b097f48bb5f9e
Ilya Stepanov <iliastepanov1996@gmail.com>
2020-05-06 18:19:22 +0200

    error handling optimized with an async middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
 //Here we have all methods that are associated with the bootcamps routes
@@ -6,89 +7,65 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc        Get all bootcamps
 // @route       /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find();
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
-    } catch (err) {
-        next(err);
-    }
-}
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
 // @desc        Get single bootcamp
 // @route       /api/v1/bootcamps/id
 // @access      Public
-exports.getBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
-        //or Bootcamp.find({ name: req.params.id })...
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    //or Bootcamp.find({ name: req.params.id })...
 
-        //if the formatting is correct but no id matching
-        if (!bootcamp) {
-            return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: bootcamp });
-
-        //if the formatting is incorrect (CastError)
-    } catch (err) {
-        next(err);
+    //if the formatting is correct but no id matching
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc        Create bootcamp
 // @route       /api/v1/bootcamps
 // @access      Private
-exports.createBootcamp = async (req, res, next) => {
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
     /*Create a document in the collection bootcamps basically
     Mongoose always returns a Promise, so await it */
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
+    const bootcamp = await Bootcamp.create(req.body);
 
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
-    } catch (err) {
-        next(err);
-    }
-}
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
+});
 
 // @desc        Update bootcamp
 // @route       /api/v1/bootcamps/id
 // @access      Private
-exports.updateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        });
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
 
-        if (!bootcamp) {
-            return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: bootcamp });
-
-    } catch (err) {
-        next(err);
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: bootcamp });
+});
 
 // @desc        Delete bootcamp
 // @route       /api/v1/bootcamps/id
 // @access      Private
-exports.deleteBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if (!bootcamp) {
-            return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
-        }
-
-        res.status(200).json({ success: true, data: {} });
-
-    } catch (err) {
-        next(err);
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Resource not found with id ${req.params.id}`, 404));
     }
-}
+
+    res.status(200).json({ success: true, data: {} });
+});
---
+++ middleware/async.js
@@ -0,0 +1,8 @@
+/*Instead of using try/catch is the controllers for error handling,
+we can optimize it using this middleware.
+The catch is ofc still calling the error middleware in case of wrong formatting (CastError)*/
+
+const asyncHandler = fn => (req, res, next) =>
+    Promise.resolve(fn(req, res, next)).catch(next)
+
+module.exports = asyncHandler;
